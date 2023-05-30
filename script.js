c9e6f7c262090fc771f775d5c690444ce6d2317a
Devvrat Singh Rathod <73027078+Devvrat1010@users.noreply.github.com>
2023-05-31 01:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 let playerone=true
 let boxes=document.querySelectorAll(".box")
-let box=document.getElementsByClassName("box")
-// box[0].style.color="Green"
 let btn=document.getElementById("button")
-console.log(document.getElementsByClassName("one")[0].innerText)
-console.log(box[1])
-console.log(box)
 function turn(element){
-    // let key=document.querySelector
     if (playerone){
         if(!(element.classList.contains("true"))){
             element.classList.add("input")
             element.classList.add("true")
             element.innerText="X"
-            // console.log(element)
             if  (checkResult("X",element)){
-                return true
             }
 
             playerone =!playerone
@@ -35,8 +27,6 @@ function turn(element){
             playerone =!playerone
         }
     }
-    return false
-    // else element.innerText="O"
 
 }
 
@@ -44,7 +34,6 @@ for(let i=0;i<9;i++){
     console.log(boxes[i])
     boxes[i].addEventListener("click",function(){
         if (turn(boxes[i])){
-            return 
         }
     })
 }
@@ -63,8 +52,8 @@ function checkResult(X,element){
         [2,5,8],
         [2,4,6],
     ]
-    // console.log(element)
     let win="true"
+
     let obj={
         one:0,
         two:1,
@@ -77,20 +66,11 @@ function checkResult(X,element){
         nine:8
     }
 
-    let winnerClass={
-
-    }
-
     let classes=element.classList
-    console.log(classes[0])
-    let first=obj[classes[0]]
-    console.log(first)
-    let target=first
+    let target=obj[classes[0]]
     
     let search=document.getElementsByClassName("box")
-    let see=search[target]
-    // console.log(see) 
-    // console.log("hehe")
+
     let x=0
     if ((target==0) || (target==2) ||(target==6) ||(target==8)){
        x=3
@@ -101,9 +81,6 @@ function checkResult(X,element){
     else {
         x=2
     }
-    let inClass="loser"
-    // see.classList.add("winner")
-    console.log("x "+x)
     let curr=0
     for (let i=0;i<8;i++){
         for (let j=0;j<3;j++){
@@ -124,20 +101,14 @@ function checkResult(X,element){
                     }
                 }
                 if (win=="true"){
+                    for (let n=0;n<3;n++){
+                        search[winningPos[i][n]].classList.add("winner")
+                    }
                     let res=document.getElementsByClassName("result")[0]
                     let ans=search[winningPos[i][curr]].innerText
                     res.innerText="Winner " + ans
-                    return true
-                    // console.log(search[winningPos[i][curr]].innerText + "   won")
-                    // see.classList.add(inClass)
                 }
-                // console.log("win "+winningPos[i])
-
-            }
-            else{
-                continue
             }
         }
     }
-    return false
 }
